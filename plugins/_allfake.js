23de549b01c645bd97948669b150473460e75943
Leonel <166152300+leoneloficial@users.noreply.github.com>
2025-12-20 01:54:59 -0400

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -8,7 +8,7 @@ const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
 var handler = m => m
 handler.all = async function (m) { 
 global.canalIdM = ["120363324350463849@newsletter", "120363324350463849@newsletter"]
-global.canalNombreM = ["ᥫ᭡sմოí́ - cαղαl - օբícíαlᰔᩚ", "ᥫ᭡sմოí́ - cαղαl - օբícíαlᰔᩚᥣׁׅ֪"]
+global.canalNombreM = [" cαղαl - օբícíαlᰔ»  ⊹˚୨ •(=^●ω●^=)•", "cαղαl - օբícíαl»  ⊹˚୨ •(=^●ω●^=)•ᥣׁׅ֪"]
 global.channelRD = await getRandomChannel()
 
 global.d = new Date(new Date + 3600000)
