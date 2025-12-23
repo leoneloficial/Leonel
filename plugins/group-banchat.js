1ffe6d0fedff7f677876574d5ff082a7214d48fd
Ado <meadonix@gmail.com>
2025-12-23 11:40:49 -0600

    Actualizar group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -3,7 +3,7 @@ import path from 'path'
 
 const handler = async (m, { conn, isAdmin, isROwner }) => {
   const senderNumber = m.sender.replace(/[^0-9]/g, '')
-  const botPath = path.join('./JadiBots', senderNumber)
+  const botPath = path.join('./Sessions/SubBot', senderNumber)
 
   if (!(isAdmin || isROwner || fs.existsSync(botPath))) {
     return m.reply(
