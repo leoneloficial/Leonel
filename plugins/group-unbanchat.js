99ca9b79a2c76c3f60e31942c9f467ea0270cbf2
Ado <meadonix@gmail.com>
2025-12-23 12:06:27 -0600

    Actualizar group-unbanchat.js

--- plugins/group-unbanchat.js
+++ plugins/group-unbanchat.js
@@ -1,27 +1,27 @@
-import fs from 'fs'
-import path from 'path'
+import fs from "fs"
+import path from "path"
 
-const handler = async (m, { conn, isAdmin, isROwner }) => {
-  const senderNumber = m.sender.replace(/[^0-9]/g, '')
-  const botPath = path.join('./Sessions/SubBot', senderNumber)
+const handler = async (m, { isAdmin, isROwner }) => {
+  const senderNumber = (m.sender || "").replace(/[^0-9]/g, "")
+  const botPath = path.join("./Sessions/SubBot", senderNumber)
+  const chatId = m.chat || m.key?.remoteJid || ""
 
   if (!(isAdmin || isROwner || fs.existsSync(botPath))) {
-    return m.reply(
-      `𖣣ֶㅤ֯⌗ No tienes permisos para usar este comando.`, 
-      m
-    )
+    return m.reply(`𖣣ֶㅤ֯⌗ No tienes permisos para usar este comando.`, m)
   }
 
-  global.db.data.chats[m.chat].isBanned = false
+  if (!chatId) return
 
-  m.reply(
-    `˚∩ El bot ha sido desbaneado correctamente.`
-  )
+  if (!global.db.data.chats[chatId]) global.db.data.chats[chatId] = {}
+  global.db.data.chats[chatId].isBanned = false
+  await global.db.write?.().catch(() => {})
+
+  m.reply(`˚∩ El bot ha sido desbaneado correctamente.`)
 }
 
-handler.help = ['desbanearbot']
-handler.tags = ['group']
-handler.command = ['desbanearbot', 'unbanchat']
+handler.help = ["desbanearbot"]
+handler.tags = ["group"]
+handler.command = ["desbanearbot", "unbanchat"]
 handler.group = true
 
 export default handler
