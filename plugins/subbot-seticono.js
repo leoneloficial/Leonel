15d1ef688ddef6890404eb6b4744ae5575cbfec5
root <root@soymaycol.soymaycol>
2025-12-23 01:23:40 +0000

    Add profile picture update for sub-bots in seticono command

--- plugins/subbot-seticono.js
+++ plugins/subbot-seticono.js
@@ -13,6 +13,7 @@ let handler = async (m, { conn }) => {
       return m.reply(`✦ El archivo enviado no es una imagen válida.`);
     }
     conn.icono = `${link}`;
+      await conn.updateProfilePicture(conn.user.jid, media);
       await conn.sendFile(m.chat, media, 'icono.jpg', `❀ Icono actualizado.`, m);
   } catch (error) {
     console.error(error);
