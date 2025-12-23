ec7a46bc890104a73718ad589a36067c44554da8
Ado <meadonix@gmail.com>
2025-12-23 12:40:34 -0600

    Actualizar group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -2,9 +2,11 @@ import fs from "fs"
 import path from "path"
 
 const handler = async (m, { isAdmin, isROwner }) => {
+  const conn = this
   const senderNumber = (m.sender || "").replace(/[^0-9]/g, "")
   const botPath = path.join("./Sessions/SubBot", senderNumber)
-  const chatId = m.chat || m.key?.remoteJid || ""
+  const rawChatId = m.chat || m.key?.remoteJid || ""
+  const chatId = typeof conn?.decodeJid === "function" ? conn.decodeJid(rawChatId) : rawChatId
 
   if (!(isAdmin || isROwner || fs.existsSync(botPath))) {
     return m.reply(`𖣣ֶㅤ֯⌗ No tienes permisos esto solo lo pueden usar *admins* *sockets* o el *owner.*`, m)
@@ -14,6 +16,9 @@ const handler = async (m, { isAdmin, isROwner }) => {
 
   if (!global.db.data.chats[chatId]) global.db.data.chats[chatId] = {}
   global.db.data.chats[chatId].isBanned = true
+  if (rawChatId && rawChatId !== chatId) {
+    global.db.data.chats[rawChatId] = global.db.data.chats[chatId]
+  }
   await global.db.write?.().catch(() => {})
 
   m.reply(`> ⌗ Bot baneado correctamente en este grupo.`)
