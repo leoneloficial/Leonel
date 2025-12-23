77f88a4364cb32da9063bef5b11929bd39e7ac5a
Ado <meadonix@gmail.com>
2025-12-23 14:16:58 -0600

    Actualizar _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -1,113 +1,48 @@
-import pkg from "@whiskeysockets/baileys"
-import fs from "fs"
-import fetch from "node-fetch"
-import axios from "axios"
-import moment from "moment-timezone"
-
+import pkg from '@whiskeysockets/baileys'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone'
 const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
 
-var handler = (m) => m
+var handler = m => m
+handler.all = async function (m) { 
+global.canalIdM = ["120363401404146384@newsletter", "120363401404146384@newsletter"]
+global.canalNombreM = ["ᥫ᭡ 𝒀𝒖𝒌𝒊 𝑾𝒂𝑩𝒐𝒕 - 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 ᯓᡣ𐭩", "ᥫ᭡ 𝒀𝒖𝒌𝒊 𝑾𝒂𝑩𝒐𝒕 - 𝑶𝒇𝒇𝒊𝒄𝒊𝒂𝒍 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 ᯓᡣ𐭩"]
+global.channelRD = await getRandomChannel()
 
-handler.all = async function (m) {
-  const conn = this
+global.d = new Date(new Date + 3600000)
+global.locale = 'es'
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
+global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
+global.mes = d.toLocaleDateString('es', {month: 'long'})
+global.año = d.toLocaleDateString('es', {year: 'numeric'})
+global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
 
-  const botname = conn.botName || global.botname || global.namebot || "Bot"
-  const icono = conn.icono || global.icono
+var canal = 'https://whatsapp.com/channel/0029VbAfPu9BqbrEMFWXKE0d'  
+var comunidad = 'https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa'
+var git = 'https://github.com/The-King-Destroy'
+var github = 'https://github.com/The-King-Destroy/Yuki_Suou-Bot' 
+var correo = 'thekingdestroy507@gmail.com'
+global.redes = [canal, comunidad, git, github, correo].getRandom()
 
-  global.canalIdM = ["120363324350463849@newsletter", "120363324350463849@newsletter"]
-  global.canalNombreM = ["»  ⊹˚୨ •(=^●ω●^=)• ❀ canal - oficial ❀", "»  ⊹˚୨ •(=^●ω●^=)• ❀ canal - oficial ❀"]
+global.nombre = m.pushName || 'Anónimo'
+global.packsticker = `°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\nᰔᩚ Usuario: ${nombre}\n❀ Bot: ${botname}\n✦ Fecha: ${fecha}\nⴵ Hora: ${moment.tz('America/Caracas').format('HH:mm:ss')}`
+global.packsticker2 = `\n°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°\n\n${dev}`
 
-  global.channelRD = await getRandomChannel()
-
-  const d = new Date(Date.now() + 3600000)
-  const locale = "es"
-
-  global.d = d
-  global.locale = locale
-  global.dia = d.toLocaleDateString(locale, { weekday: "long" })
-  global.fecha = d.toLocaleDateString("es", { day: "numeric", month: "numeric", year: "numeric" })
-  global.mes = d.toLocaleDateString("es", { month: "long" })
-  global.año = d.toLocaleDateString("es", { year: "numeric" })
-  global.tiempo = d.toLocaleString("en-US", {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    hour12: true,
-  })
-
-  const canal = "https://whatsapp.com/channel/0029VbAc6cS002TEZ4r5261E"
-  const comunidad = "https://chat.whatsapp.com/I0dMp2fEle7L6RaWBmwlAa"
-  const git = "https://github.com/The-King-Destroy"
-  const github = "https://github.com/The-King-Destroy/Yuki_Suou-Bot"
-  const correo = "thekingdestroy507@gmail.com"
-
-  global.redes = pickRandom([canal, comunidad, git, github, correo])
-
-  global.nombre = m.pushName || "Anónimo"
-  global.packsticker = `\n👑 Usuario: ${global.nombre}\n🤍 Bot: ${botname}\n🐋 Fecha: ${global.fecha}\n🕐 Hora: ${moment.tz("America/Caracas").format("HH:mm:ss")}`
-  global.packsticker2 = ``
-
-  const senderNum = (m.sender || "").split("@")[0]
-
-  global.fkontak = {
-    key: {
-      participants: "0@s.whatsapp.net",
-      remoteJid: "status@broadcast",
-      fromMe: false,
-      id: "Halo",
-    },
-    message: {
-      contactMessage: {
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${senderNum}:${senderNum}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
-      },
-    },
-    participant: "0@s.whatsapp.net",
-  }
-
-  const dev = global.dev || global.developer || "Dev"
-
-  let thumb = null
-  try {
-    if (icono && /^https?:\/\//i.test(icono)) thumb = await (await fetch(icono)).buffer()
-  } catch {}
-
-  global.rcanal = {
-    contextInfo: {
-      isForwarded: true,
-      forwardedNewsletterMessageInfo: {
-        newsletterJid: global.channelRD.id,
-        serverMessageId: "",
-        newsletterName: global.channelRD.name,
-      },
-      externalAdReply: {
-        title: botname,
-        body: dev,
-        mediaUrl: null,
-        description: null,
-        previewType: "PHOTO",
-        thumbnail: thumb,
-        sourceUrl: global.redes,
-        mediaType: 1,
-        renderLargerThumbnail: false,
-      },
-      mentionedJid: null,
-    },
-  }
+global.fkontak = { key: { participants:"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: '', newsletterName: channelRD.name }, externalAdReply: { title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnail: await (await fetch(icono)).buffer(), sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, mentionedJid: null }}
 }
 
 export default handler
 
 function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
+return list[Math.floor(Math.random() * list.length)]
 }
 
 async function getRandomChannel() {
-  const ids = global.canalIdM || []
-  const names = global.canalNombreM || []
-  const randomIndex = Math.floor(Math.random() * ids.length)
-
-  const id = ids[randomIndex]
-  const name = names[randomIndex] || ""
-
-  return { id, name }
+let randomIndex = Math.floor(Math.random() * canalIdM.length)
+let id = canalIdM[randomIndex]
+let name = canalNombreM[randomIndex]
+return { id, name }
 }
